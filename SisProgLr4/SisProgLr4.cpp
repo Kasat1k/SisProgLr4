948b24462297fac7abf0ac3f9bbf00c69249934a
2 <2@DESKTOP-08SS7DI>
2023-12-04 22:58:21 +0200

    Add project files.

---
+++ SisProgLr4/SisProgLr4.cpp
@@ -0,0 +1,414 @@
+﻿#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+using ParsingTable = map<string, map<string, string>>;
+using Productions = vector<string>;
+using ParsingStack = stack<string>;
+
+map<string, Productions> readGrammar() {
+    map<string, Productions> grammar;
+    grammar["E"] = { "TF" };
+    grammar["F"] = { "+TF", "ε" };
+    grammar["T"] = { "GU" };
+    grammar["U"] = { "*GU", "ε" };
+    grammar["G"] = { "(E)", "i" };
+    return grammar;
+}
+
+bool isTerminal(const string& symbol) {
+    const set<string> terminals = { "i", "+", "*", "(", ")", "?" };
+    return terminals.find(symbol) != terminals.end();
+}
+
+bool ll1Parse(const string& input, const ParsingTable& table, const string& start_symbol) {
+    ParsingStack pstack;
+    pstack.push("$");
+    pstack.push(start_symbol);
+
+    size_t input_pos = 0;
+    while (!pstack.empty()) {
+        string top = pstack.top();
+        pstack.pop();
+
+        string current_input_symbol = (input_pos < input.size()) ? string(1, input[input_pos]) : "$";
+        cout << "[DEBUG] Top of the stack: " << top << ", Current input symbol: " << current_input_symbol << endl;
+
+        if (top == "$" && current_input_symbol == "$") {
+            continue;
+        }
+        else if (isTerminal(top)) {
+            if (top == current_input_symbol) {
+                input_pos++;
+            }
+            else {
+                cout << "[ERROR] Terminal mismatch. Expected: " << top << ", Found: " << current_input_symbol << endl;
+                return false;
+            }
+        }
+        else {
+            auto inner_map = table.find(top);
+            if (inner_map != table.end()) {
+                auto table_entry = inner_map->second.find(current_input_symbol);
+                if (table_entry != inner_map->second.end()) {
+                    string production = table_entry->second;
+                    if (production != "?") {
+                        for (int i = production.size() - 1; i >= 0; i--) {
+                            pstack.push(string(1, production[i]));
+                        }
+                    }
+                }
+                else {
+                    cout << "[ERROR] Parsing table doesn't have an entry for [" << top << ", " << current_input_symbol << "]" << endl;
+                    return false;
+                }
+            }
+            else {
+                cout << "[ERROR] Parsing table doesn't have an entry for [" << top << "]" << endl;
+                return false;
+            }
+        }
+    }
+    return input_pos == input.size();
+}
+
+// Эпсилон-нетерминалы
+set<string> findEpsilonNonTerminals(const map<string, Productions>& grammar) {
+    map<string, bool> epsilon_status;
+    for (const auto& rule : grammar) {
+        epsilon_status[rule.first] = false;
+    }
+
+    bool changed = true;
+    while (changed) {
+        changed = false;
+        for (const auto& rule : grammar) {
+            for (const string& production : rule.second) {
+                bool allEpsilon = true;
+                for (char ch : production) {
+                    if (isTerminal(string(1, ch)) || (!epsilon_status[string(1, ch)] && !isTerminal(string(1, ch)))) {
+                        allEpsilon = false;
+                        break;
+                    }
+                }
+                if (allEpsilon) {
+                    if (!epsilon_status[rule.first]) {
+                        epsilon_status[rule.first] = true;
+                        changed = true;
+                    }
+                }
+            }
+        }
+    }
+
+    set<string> epsilon_non_terminals;
+    for (const auto& item : epsilon_status) {
+        if (item.second) {
+            epsilon_non_terminals.insert(item.first);
+        }
+    }
+    return epsilon_non_terminals;
+}
+
+// Подсчет FirstK
+map<string, set<string>> computeFirstK(const map<string, Productions>& grammar, const set<string>& epsilon_non_terminals, int k) {
+    map<string, set<string>> first_k_sets;
+
+    for (const auto& entry : grammar) {
+        first_k_sets[entry.first] = {};
+    }
+
+    bool changed = true;
+    while (changed) {
+        changed = false;
+
+        for (const auto& rule : grammar) {
+            const string& non_terminal = rule.first;
+            for (const string& production : rule.second) {
+                vector<set<string>> first_k_for_symbols;
+
+                for (char symbol : production) {
+                    if (isTerminal(string(1, symbol))) {
+                        first_k_for_symbols.push_back({ string(1, symbol) });
+                    }
+                    else {
+                        first_k_for_symbols.push_back(first_k_sets[string(1, symbol)]);
+                    }
+                }
+
+                set<string> new_first_k;
+                bool break_outer = false;
+
+                for (int i = 0; i < first_k_for_symbols.size() && !break_outer; ++i) {
+                    if (i == 0) {
+                        new_first_k = first_k_for_symbols[i];
+                    }
+                    else {
+                        set<string> temp;
+                        for (const auto& a : new_first_k) {
+                            for (const auto& b : first_k_for_symbols[i]) {
+                                string combined = a + b;
+                                if (combined.size() > k) {
+                                    combined = combined.substr(0, k);
+                                }
+                                temp.insert(combined);
+                            }
+                        }
+                        new_first_k = temp;
+                    }
+
+                    if (epsilon_non_terminals.find(string(1, production[i])) == epsilon_non_terminals.end()) {
+                        break_outer = true;
+                    }
+                }
+
+                for (const string& item : new_first_k) {
+                    if (first_k_sets[non_terminal].insert(item).second) {
+                        changed = true;
+                    }
+                }
+            }
+        }
+    }
+
+    return first_k_sets;
+}
+
+// Подсчет FollowK
+map<string, set<string>> computeFollowK(const map<string, Productions>& grammar, const map<string, set<string>>& first_k_sets, int k) {
+    map<string, set<string>> follow_k_sets;
+    for (const auto& rule : grammar) {
+        follow_k_sets[rule.first] = {};
+    }
+    follow_k_sets["E"].insert("$");
+
+    bool changed = true;
+    while (changed) {
+        changed = false;
+        for (const auto& rule : grammar) {
+            for (const string& production : rule.second) {
+                set<string> current_follow;
+                for (int i = production.size() - 1; i >= 0; i--) {
+                    char symbol = production[i];
+                    string symStr(1, symbol);
+                    if (isTerminal(symStr)) {
+                        current_follow = { symStr };
+                    }
+                    else {
+                        if (follow_k_sets.find(symStr) != follow_k_sets.end()) {
+                            follow_k_sets[symStr].insert(current_follow.begin(), current_follow.end());
+                        }
+                        if (first_k_sets.find(symStr) != first_k_sets.end() && first_k_sets.at(symStr).count("ε")) {
+                            set<string> temp_first = first_k_sets.at(symStr);
+                            temp_first.erase("ε");
+                            current_follow.insert(temp_first.begin(), temp_first.end());
+                        }
+                        else if (first_k_sets.find(symStr) != first_k_sets.end()) {
+                            current_follow = first_k_sets.at(symStr);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return follow_k_sets;
+}
+
+// Построение таблицы разбора для LL(k)
+ParsingTable buildParsingTableK(const map<string, Productions>& grammar, const map<string, set<string>>& first_k_sets, const map<string, set<string>>& follow_k_sets, int k) {
+    ParsingTable table;
+
+    for (const auto& rule : grammar) {
+        const string& non_terminal = rule.first;
+        for (const string& production : rule.second) {
+            set<string> first_of_production;
+
+            bool contains_epsilon = true;
+            for (char symbol : production) {
+                if (isTerminal(string(1, symbol))) {
+                    first_of_production.insert(string(1, symbol));
+                    contains_epsilon = false;
+                    break;
+                }
+                else {
+                    for (const string& terminal : first_k_sets.at(string(1, symbol))) {
+                        if (terminal != "ε") {
+                            first_of_production.insert(terminal);
+                        }
+                    }
+                    if (first_k_sets.at(string(1, symbol)).count("ε") == 0) {
+                        contains_epsilon = false;
+                        break;
+                    }
+                }
+            }
+
+            if (contains_epsilon) {
+                first_of_production.insert("ε");
+            }
+
+            for (const string& terminal : first_of_production) {
+                if (terminal != "ε") {
+                    table[non_terminal][terminal] = production;
+                }
+                else {
+                    for (const string& terminal_in_follow : follow_k_sets.at(non_terminal)) {
+                        table[non_terminal][terminal_in_follow] = production;
+                    }
+                }
+            }
+        }
+    }
+
+    return table;
+}
+
+class ASTNode {
+public:
+    string value;
+    vector<ASTNode*> children;
+
+    ASTNode(const string& val) : value(val) {}
+
+    void AddChild(ASTNode* child) {
+        children.push_back(child);
+    }
+
+    void Display(int level = 0) const {
+        cout << string(level * 2, ' ') << value << endl;
+        for (auto child : children) {
+            child->Display(level + 1);
+        }
+    }
+
+    ~ASTNode() {
+        for (auto child : children) {
+            delete child;
+        }
+    }
+};
+
+ASTNode* BuildAST(const string& input, const ParsingTable& table, const string& start_symbol) {
+    ParsingStack pstack;
+    pstack.push("$");
+    pstack.push(start_symbol);
+
+    ASTNode* astRoot = new ASTNode(start_symbol);
+    ASTNode* current = astRoot;
+
+    size_t input_pos = 0;
+    while (!pstack.empty()) {
+        string top = pstack.top();
+        pstack.pop();
+
+        string current_input_symbol = (input_pos < input.size()) ? string(1, input[input_pos]) : "$";
+
+        if (top == "$" && current_input_symbol == "$") {
+            continue;
+        }
+        else if (isTerminal(top)) {
+            if (top == current_input_symbol) {
+                input_pos++;
+                ASTNode* terminalNode = new ASTNode(current_input_symbol);
+                current->AddChild(terminalNode);
+            }
+        }
+        else {
+            auto inner_map = table.find(top);
+            if (inner_map != table.end()) {
+                auto table_entry = inner_map->second.find(current_input_symbol);
+                if (table_entry != inner_map->second.end()) {
+                    string production = table_entry->second;
+                    if (production != "?") {
+                        ASTNode* nonTerminalNode = new ASTNode(top);
+                        current->AddChild(nonTerminalNode);
+                        current = nonTerminalNode;
+
+                        for (int i = production.size() - 1; i >= 0; i--) {
+                            pstack.push(string(1, production[i]));
+                        }
+                    }
+                }
+            }
+        }
+
+        // Move to the parent if the current node has no more children
+        while (!current->children.empty() && input_pos == input.size()) {
+            current = current->children.back();
+        }
+    }
+
+    return astRoot;
+}
+
+
+void DisplayAST(ASTNode* root) {
+    root->Display();
+}
+
+int main() {
+    auto grammar = readGrammar();
+    auto epsilon_non_terminals = findEpsilonNonTerminals(grammar);
+    int k = 2;
+    auto first_k_sets = computeFirstK(grammar, epsilon_non_terminals, k);
+    auto follow_k_sets = computeFollowK(grammar, first_k_sets, k);
+
+    cout << "First sets:" << endl;
+    for (const auto& item : first_k_sets) {
+        cout << "First(" << item.first << ") = {";
+        for (const string& val : item.second) {
+            cout << " " << val;
+        }
+        cout << " }" << endl;
+    }
+
+    cout << "\nFollow sets:" << endl;
+    for (const auto& item : follow_k_sets) {
+        cout << "Follow(" << item.first << ") = {";
+        for (const string& val : item.second) {
+            cout << " " << val;
+        }
+        cout << " }" << endl;
+    }
+
+    ParsingTable table = buildParsingTableK(grammar, first_k_sets, follow_k_sets, k);
+    table["U"]["+"] = "ε";
+    table["U"]["$"] = "ε";
+    table["U"][")"] = "ε";
+    table["F"][")"] = "ε";
+    table["F"]["$"] = "ε";
+
+    cout << "\nParsing Table:\n";
+    for (const auto& row : table) {
+        for (const auto& entry : row.second) {
+            cout << "On [" << row.first << ", " << entry.first << "] apply: " << entry.second << "\n";
+        }
+    }
+
+    vector<string> testInput = {
+        "i+i*i",
+        "i*i+i",
+        "(i+i)*i",
+        "i+(i+i)",
+        "i+i+i"
+    };
+    for (const string& test_input : testInput) {
+        bool result = ll1Parse(test_input, table, "E");
+        cout << "Parsing input '" << test_input << "': " << (result ? "SUCCESS" : "FAILED") << endl;
+
+        if (result) {
+            ASTNode* astRoot = BuildAST(test_input, table, "E");
+            cout << "\nAbstract Syntax Tree (AST) for input '" << test_input << "':\n";
+            DisplayAST(astRoot);
+            delete astRoot; // Очистка пам'яті після відображення AST
+        }
+    }
+
+    return 0;
+}
